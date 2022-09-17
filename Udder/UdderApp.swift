5c75116d25e287e6b9d318be252e5f54ee92d98b
Angel Rincon <aa17rv@gmail.com>
2022-09-17 14:25:22 -0400

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Angel Rincon on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
