cc6fc0c067a3abfb3d042e78b48a138363b1b01b
Angel Rincon <aa17rv@gmail.com>
2022-09-17 14:50:48 -0400

    add labels

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -10,10 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Text("Udder")
+                .font(.title)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .dynamicTypeSize(.xxxLarge)
+                .offset(y: -300)
+            Text("Fresh Milk, On Demand.")
+                .font(.footnote)
+                .multilineTextAlignment(.center)
+                .offset(y: -290)
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Image(systemName: "eyes")
         }
         .padding()
     }
